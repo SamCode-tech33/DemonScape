763f09836dfd2d550f5ceb13146258910320ba86
Sam <samharaway33@gmail.com>
2025-08-07 02:28:03 +0900

    simple change to the start screen and to trigger vercel

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Dungeon Game</h1>
+    <main className="flex flex-col items-center justify-around h-screen">
+      <h1 className="text-4xl font-bold text-red-950 underline">Demon Scape</h1>
       <Link href="/game">
-        <button className="mt-4 px-6 py-2 bg-blue-600 text-white rounded">
+        <button className="text-3xl h-24 w-48 bg-red-950 text-black rounded-lg hover:text-white hover:bg-red-800 cursor-pointer">
           Play Game
         </button>
       </Link>
