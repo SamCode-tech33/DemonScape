a329b916891b9eebb506a98947d44287364567ce
Sam <samharaway33@gmail.com>
2025-08-12 22:40:13 +0900

    added a health bar, magic bar, and update functionality to update health on hit when the fight mechanics are done. Also added a controls information box that can be hidden with 'h'. Lastly fixed the gold on hover not working on the main page.

--- app/components/CanvasOne.tsx
+++ app/components/CanvasOne.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useRef } from "react";
 import Phaser from "phaser";
 import SceneOne from "../phaser/SceneOne";
+import SceneHud from "../phaser/SceneHud";
 
 export default function CanvasOne() {
   const gameRef = useRef<Phaser.Game | null>(null);
@@ -12,14 +13,15 @@ export default function CanvasOne() {
 
     const config: Phaser.Types.Core.GameConfig = {
       type: Phaser.AUTO,
-      width: 2720,
+      width: 2555,
       height: 1280,
       parent: "gameCanvas",
+      transparent: true,
       physics: {
         default: "arcade",
         arcade: { gravity: { y: 0, x: 0 }, debug: true },
       },
-      scene: [SceneOne],
+      scene: [SceneOne, SceneHud],
     };
 
     gameRef.current = new Phaser.Game(config);
