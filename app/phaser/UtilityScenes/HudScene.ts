5555dce9979bb9d6317326a91356c8b95f5d5daf
Sam <samharaway33@gmail.com>
2026-09-10 17:56:45 +0900

    moved enemy HUD to updated location

--- app/phaser/UtilityScenes/HudScene.ts
+++ app/phaser/UtilityScenes/HudScene.ts
@@ -246,14 +246,14 @@ export default class HudScene extends Phaser.Scene {
     this.enemyHealthBarBg.lineStyle(2, 0xffd700, 1);
     this.enemyHealthBarBg.fillRoundedRect(
       screenWidth - 32 - 272,
-      24,
+      10,
       272,
       30,
       radius,
     );
     this.enemyHealthBarBg.strokeRoundedRect(
       screenWidth - 32 - 272,
-      24,
+      10,
       272,
       30,
       radius,
@@ -263,14 +263,14 @@ export default class HudScene extends Phaser.Scene {
     this.enemyMagicBarBg.fillStyle(0x555555, 1);
     this.enemyMagicBarBg.fillRoundedRect(
       screenWidth - 340 - 272,
-      24,
+      10,
       272,
       30,
       radius,
     );
     this.enemyMagicBarBg.strokeRoundedRect(
       screenWidth - 340 - 272,
-      24,
+      10,
       272,
       30,
       radius,
@@ -279,7 +279,7 @@ export default class HudScene extends Phaser.Scene {
     this.enemyHealthText = this.add
       .text(
         screenWidth - 32 - 242,
-        28,
+        14,
         `HP: ${this.enemyHealth}/${this.enemyMaxHealth}`,
         { fontSize: "20px", color: "#fff" },
       )
@@ -288,7 +288,7 @@ export default class HudScene extends Phaser.Scene {
     this.enemyMagicText = this.add
       .text(
         screenWidth - 340 - 242,
-        28,
+        14,
         `MP: ${this.enemyMagic}/${this.enemyMaxMagic}`,
         { fontSize: "20px", color: "#fff" },
       )
@@ -306,9 +306,9 @@ export default class HudScene extends Phaser.Scene {
       this.enemyHealthBar.fillStyle(0xff0000, 1);
       this.enemyHealthBar.fillRoundedRect(
         screenWidth - 32 - 266 * (this.enemyHealth / this.enemyMaxHealth) - 4,
-        25,
+        12,
         268 * (this.enemyHealth / this.enemyMaxHealth),
-        28,
+        26,
         radius,
       );
 
@@ -316,9 +316,9 @@ export default class HudScene extends Phaser.Scene {
       this.enemyMagicBar.fillStyle(0x0000ff, 1);
       this.enemyMagicBar.fillRoundedRect(
         screenWidth - 340 - 266 * (this.enemyMagic / this.enemyMaxMagic) - 4,
-        25,
+        12,
         268 * (this.enemyMagic / this.enemyMaxMagic),
-        28,
+        26,
         radius,
       );
     } else {
